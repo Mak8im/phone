fdae4d47186768706133f61c0ceb444e1c3cb5e0
Mak8im <142336958+Mak8im@users.noreply.github.com>
2025-02-19 03:05:17 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        // Рассылаем сообщение всем подключенным клиентам
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.send('Добро пожаловать в чат!');
+});
